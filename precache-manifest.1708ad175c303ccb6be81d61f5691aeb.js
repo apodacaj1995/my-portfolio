29f831e546c509c0b0e5e8e060806bb73930f87e
apodacaj1995 <jaclynkapodaca@gmail.com>
2020-08-09 17:59:36 -0600

    Updates

---
+++ precache-manifest.1708ad175c303ccb6be81d61f5691aeb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55024e8157dfd2d2d41f6ee65166cee",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "561a6e5792aaeba7f908",
+    "url": "/my-portfolio/static/css/main.a3d7493e.chunk.css"
+  },
+  {
+    "revision": "b5ff7aea8ebdd5af5bcc",
+    "url": "/my-portfolio/static/js/2.a330f119.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/my-portfolio/static/js/2.a330f119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561a6e5792aaeba7f908",
+    "url": "/my-portfolio/static/js/main.04a4d0cd.chunk.js"
+  },
+  {
+    "revision": "11a9f05f57c9e93d27fe",
+    "url": "/my-portfolio/static/js/runtime-main.1eb8a9a3.js"
+  },
+  {
+    "revision": "93d289e2840974895bf2ee31df57d583",
+    "url": "/my-portfolio/static/media/Headshot.93d289e2.jpg"
+  },
+  {
+    "revision": "436dec928c8331e20d03db27629bdc8a",
+    "url": "/my-portfolio/static/media/design.436dec92.png"
+  },
+  {
+    "revision": "04ebd5b8c92db1273a23a4d22808b85e",
+    "url": "/my-portfolio/static/media/fun-stuff.04ebd5b8.png"
+  },
+  {
+    "revision": "670367fb3d15b9e2ed2518137281a728",
+    "url": "/my-portfolio/static/media/html.670367fb.jpg"
+  },
+  {
+    "revision": "da34367199daffca08149a10935ffd81",
+    "url": "/my-portfolio/static/media/laptop.da343671.png"
+  },
+  {
+    "revision": "9150a4637d723cb2ed397220361a2b04",
+    "url": "/my-portfolio/static/media/logo.9150a463.png"
+  },
+  {
+    "revision": "76c095b6819308b6b89816ecc62c74d5",
+    "url": "/my-portfolio/static/media/logos.76c095b6.png"
+  },
+  {
+    "revision": "a2a9f57cde2ad91dfd2655a7a53a87e4",
+    "url": "/my-portfolio/static/media/tablet2.a2a9f57c.jpg"
+  }
+]);
